47cf5219d9d8fb9fa04e15f2107a2501b041baf8
Nick Morley <nick.morley111@gmail.com>
2018-06-02 11:31:49 +0100

    .

--- __init__.py
+++ __init__.py
@@ -1,2 +1,6 @@
+import os
+import sys
+cwd = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(cwd)
 from parse_pmc import PMCArticleSet, PMCArticle
 from parse_pubmed import PubMedArticleSet, PubMedArticle
